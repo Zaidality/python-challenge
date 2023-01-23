3042af7fa67ae589aa975afecb7da433d4b54bf9
Zaidality <zsarmad95@gmail.com>
2023-01-23 07:34:44 -0500

    Finalizing work

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,9 @@ report_path = os.path.join('analysis','analysis_report.txt')
 # Variables
 Total_Months = 0
 Total = 0
+net_change_list = []
+Profits_max = 0
+Profits_min = 0
 
 # opening csv file
 with open(csvpath) as csvfile:
@@ -19,6 +22,7 @@ with open(csvpath) as csvfile:
     # extracting info from first row
     row1 = next(csvreader)
     Total_Months = Total_Months + 1
+    previous_net = int(row1[1])
 
     # extracting the total $
     Total += int(row1[1])
@@ -32,15 +36,29 @@ with open(csvpath) as csvfile:
         # summing total P/L of months
         Total += int(row[1])
 
+        # getting net change
+        net_change = int(row[1]) - previous_net 
+        net_change_list += [net_change]
 
+        # Finding the greatest profit increase
+        if int(row[1]) < int(row[1]) - 1:
+            Profits_max = row
 
+        # Finding the greatest profit decrease
+        if int(row[1]) < int(row[1]) - 1:
+            Profits_min = row
+
+# net monthly avg
+net_avg = sum(net_change_list) / (int(Total_Months) - 1)
 
 analysis_report = (
     f"Financial Analysis\n"
     f"---------------------------\n"
     f"Total months: {Total_Months} \n"
     f"Total: {Total} \n"
-    
+    f"Average Change: {net_avg} \n"
+    f"Greatest increase in profits: {Profits_max} \n"
+    f"Greatest decrease in profits: {Profits_min} \n"
 
 
 )
@@ -51,3 +69,4 @@ print(analysis_report)
 
 with open(report_path, 'w') as report_file:
     report_file.write(analysis_report)
+print(net_change_list)
